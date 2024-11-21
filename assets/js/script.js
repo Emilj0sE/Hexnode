e03dd230ed81d30219e0913595acf09da5329149
Emilj0sE <emiljose51ej@gmail.com>
2024-11-22 00:07:47 +0530

    Logo carousal designing finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,12 +81,51 @@ $(document).ready(function () {
       }
   });
 
-  // Slick CArousal 
+  // Review Carousal 
     $('.review-carousal').slick({
       arrows: true,
       autoplay: true,           
       autoplaySpeed: 10000,     
       infinite: false,    
+    });
+
+    // Logo carousal
+    $('.logo-carousal').slick({
+      arrows: false,
+      autoplay: true,           
+      autoplaySpeed: 1000, 
+      hoverpauseOnHover :true,
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1280,
+          settings: {
+            slidesToShow: 6,
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 4,
+          }
+        },
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 3,
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]    
     });
 
 
